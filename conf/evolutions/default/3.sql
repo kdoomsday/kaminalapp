f0298dfc00c63f69f047e830e1a465dfa3aadb68
Eduardo Barrientos <kdoomsday@gmail.com>
2017-06-26 14:48:35 -0400

    Quitar unique de la columna nombre de los clientes

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,6 +1,10 @@
 # --- !Ups
 alter table clientes add column cuenta char(20);
 
+ALTER TABLE clientes DROP CONSTRAINT clientes_nombre_key;
+
 # --- !Downs
 
 alter table clientes drop column cuenta;
+
+ALTER TABLE clientes ADD CONSTRAINT clientes_nombre_key UNIQUE(nombre);
