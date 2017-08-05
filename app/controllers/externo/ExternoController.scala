a1ba137af79da60fb005de56b01b8c4627a93a69
Eduardo Barrientos <kdoomsday@gmail.com>
2017-08-05 10:53:22 -0400

    Muestra el formulario de agregar pago con un botón en la interfaz

--- app/controllers/externo/ExternoController.scala
+++ app/controllers/externo/ExternoController.scala
@@ -3,8 +3,11 @@ package controllers.externo
 import controllers.actions.Actions
 import daos.MascotaDao
 import format.DateFormatter
+import models.PagoPendiente
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
 import javax.inject._
 import scala.concurrent.Future
 
@@ -15,11 +18,29 @@ class ExternoController @Inject() (
     actions: Actions,
     val messagesApi: MessagesApi
 ) extends Controller with I18nSupport {
+  import ExternoController._
 
   implicit val dFmt = dateFormatter
 
-  def index = actions.timedAction { implicit request ⇒
-    Future.successful(Ok(views.html.externo.home(mascotaDao.mascotasCliente(request.session("login")))))
+  def index = actions.rAction("usuario") { implicit request ⇒
+    Ok(views.html.externo.home(mascotaDao.mascotasCliente(request.session("login"))))
   }
 
+  def agregarPagoView = actions.rAction("usuario") { implicit request ⇒
+    val mascotas = mascotaDao.mascotasCliente(request.session("login"))
+    Ok(views.html.externo.pagoPendienteView(pagoPendienteForm, mascotas))
+  }
+
+}
+
+object ExternoController {
+  def pagoPendienteForm: Form[PagoPendiente] = Form(
+    mapping(
+      "id" → default(longNumber, 0L),
+      "idMascota" → longNumber,
+      "monto" → bigDecimal(16, 2),
+      "confirmacion" → optional(nonEmptyText),
+      "imagen" → optional(nonEmptyText)
+    )(PagoPendiente.apply)(PagoPendiente.unapply)
+  )
 }
