68495dc6a3a580f67f4273981df7c3dcf01b13e7
Eduardo Barrientos <kdoomsday@gmail.com>
2017-08-09 23:13:30 -0400

    Mostrar el cliente y la mascota en pagos pendientes

--- app/daos/PagosDao.scala
+++ app/daos/PagosDao.scala
@@ -1,10 +1,14 @@
 package daos
 
-import models.PagoPendiente
+import models.{ Cliente, Mascota, PagoPendiente }
 
 /** Dao para los pagos y pagos pendientes */
 trait PagosDao {
   def addPendiente(p: PagoPendiente): Unit
 
-  def verPendientes(): List[PagoPendiente]
+  /**
+   * Buscar todos los pagos pendientes junto con su mascota y el cliente
+   * dueño de la mascota
+   */
+  def verPendientes(): List[(PagoPendiente, Mascota, Cliente)]
 }
--- app/daos/doobie/PagosDaoDoobie.scala
+++ app/daos/doobie/PagosDaoDoobie.scala
@@ -2,10 +2,11 @@ package daos.doobie
 
 import doobie.imports._
 import doobie.util.transactor.DataSourceTransactor
+import daos.doobie.DoobieImports._
 import javax.inject.Inject
 import daos.PagosDao
 import play.api.db.Database
-import models.PagoPendiente
+import models.{ Cliente, Mascota, PagoPendiente }
 import play.api.Logger
 
 /** Implementación de PagosDao con Doobie */
@@ -19,7 +20,7 @@ class PagosDaoDoobie @Inject() (db: Database) extends PagosDao {
     qAddPago(p.idMascota, p.monto, p.confirmacion).run.transact(transactor).unsafePerformIO
   }
 
-  def verPendientes(): List[PagoPendiente] = {
+  def verPendientes(): List[(PagoPendiente, Mascota, Cliente)] = {
     Logger.debug(s"Consultar lista de pagos pendientes")
     qTodosPendientes.list.transact(transactor).unsafePerformIO
   }
@@ -35,5 +36,10 @@ object PagosDaoDoobie {
     sql"""insert into pago_pendiente(id_mascota, monto, confirmacion)
           values($idMascota, $monto, $confirmacion)""".update
 
-  val qTodosPendientes = sql"""select * from pago_pendiente""".query[PagoPendiente]
+  val qTodosPendientes =
+    sql"""select pp.*, m.*, c.*
+          from pago_pendiente pp
+	        join mascotas m on pp.id_mascota = m.id
+	        join clientes c on m.id_cliente = c.id
+       """.query[(PagoPendiente, Mascota, Cliente)]
 }
